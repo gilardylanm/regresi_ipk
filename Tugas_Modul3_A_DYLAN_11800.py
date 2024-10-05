d028cc41f2e9112ff934279b7dfab291e291223c
gilardylanm <220711800@students.uajy.ac.id>
2024-10-05 21:39:19 +0700

    Update Tugas_Modul3_A_DYLAN_11800.py

--- Tugas_Modul3_A_DYLAN_11800.py
+++ Tugas_Modul3_A_DYLAN_11800.py
@@ -71,8 +71,7 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #0073e6;'>Data yang diupload:</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'C:\Users\ROG STRIX\Documents\000000000000000 MESIN\Tugas3_A_11800'
-    model_path = os.path.join(model_directory, r'SVR_IPK_model.pkl')
+    model_path = r'SVR_IPK_model.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
